aeb2a7209669f01c32676ef2c2de56d5084c8199
AJRwin <arnwinrosales2@gmail.com>
2025-09-19 17:41:09 +0800

    galllery

--- pages/views.py
+++ pages/views.py
@@ -13,3 +13,14 @@ def about(request):
 
 def hello(request, name):
     return render(request, 'hello.html', {'name': name})
+
+def gallery(request):
+    # assume images placed in pages/scripts/img/
+    images= ['img1.jpg', 'img2.jpg', 'img3.jpg']
+    return render(request, 'gallery.html', {'images':images})
+
+def page_not_found_view(request, exception):
+    return render(request, '404.html', status=404)
+
+def server_error_view(request):
+    return render(request, '500.html', status=500)
